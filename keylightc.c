3cd89c81a4d82dcc42223d5bfa4876eb6729929b
Michael Marley <michael@michaelmarley.com>
2024-09-06 11:45:08 -0400

    Do timing on the main/input thread instead of a separate one
    This brings significant complexity reduction and efficiency
    improvement because the main thread now only needs to check for new
    input immediately before it would otherwise turn off the backlight.

--- keylightc.c
+++ keylightc.c
@@ -63,17 +63,11 @@ static struct option const long_options[]={
 	{},
 };
 
-static pthread_cond_t timer_cond;
-static pthread_mutex_t timer_mutex;
-
 static pthread_cond_t fader_cond;
 static pthread_mutex_t fader_mutex;
 
 static bool fade_init=false;
 static int desired_brightness=0;
-
-static bool off_time_changed=false;
-static struct timespec off_time={};
 
 static bool exit_requested=false;
 static bool is_daemon;
@@ -103,24 +97,8 @@ static void exit_handler(int const signal_number){
 	exit_requested=true;
 }
 
-static void start_fade(int const brightness){
-	pthread_mutex_lock(&fader_mutex);
-	fade_init=true;
-	desired_brightness=brightness;
-	pthread_cond_signal(&fader_cond);
-	pthread_mutex_unlock(&fader_mutex);
-}
-
 static int const is_event_device(struct dirent const *const restrict directory_entry){
 	return strncmp(EVENT_DEVICE_FILENAME_PREFIX,directory_entry->d_name,5)==0;
-}
-
-static long const min(long const x,long const y){
-	if(x<y){
-		return x;
-	}else{
-		return y;
-	}
 }
 
 static bool const string_in_array(char const *const restrict string,char const *const restrict array[],int const array_length){
@@ -178,6 +156,14 @@ static void set_brightness(FILE *const restrict brightness_file,int const bright
 	fflush(brightness_file);
 }
 
+static void start_fade(int const brightness){
+	pthread_mutex_lock(&fader_mutex);
+	fade_init=true;
+	desired_brightness=brightness;
+	pthread_cond_signal(&fader_cond);
+	pthread_mutex_unlock(&fader_mutex);
+}
+
 static int const string_to_int(int *const restrict result,int const min,int const max,char const *const restrict string){
 	errno=0;
 	const long long_value=strtol(string,NULL,10);
@@ -195,7 +181,7 @@ static void timespec_add_nsec(struct timespec *const restrict timespec,long cons
 	timespec->tv_sec+=nsec/NSEC_PER_SEC;
 	timespec->tv_nsec+=nsec%NSEC_PER_SEC;
 	if(timespec->tv_nsec>=NSEC_PER_SEC){
-		timespec->tv_sec+=1;
+		timespec->tv_sec++;
 		timespec->tv_nsec-=NSEC_PER_SEC;
 	}
 }
@@ -207,6 +193,15 @@ static int const timespec_cmp(struct timespec const ts1,struct timespec const ts
 		return 1;
 	}else{
 		return -1;
+	}
+}
+
+static void timespec_sub(struct timespec *const restrict ts1,struct timespec const ts2){
+	ts1->tv_sec-=ts2.tv_sec;
+	ts1->tv_nsec-=ts2.tv_nsec;
+	if(ts1->tv_nsec<0&&ts1->tv_sec>=1){
+		ts1->tv_sec--;
+		ts1->tv_nsec+=NSEC_PER_SEC;
 	}
 }
 
@@ -260,26 +255,6 @@ void *fader(void *const restrict arg){
 	}
 }
 
-void *timer(void *const restrict arg){
-	pthread_mutex_lock(&timer_mutex);
-	
-	int ret=0;
-	while(true){
-		if(desired_brightness==0){
-			// If the backlight is already off, wait to be signaled once it has been turned back on
-			ret=pthread_cond_wait(&timer_cond,&timer_mutex);
-		}else if(ret==ETIMEDOUT&&!off_time_changed){
-			// If the backlight is on and current_time is greater than or equal to off_time, turn the backlight off
-			log_write(LOG_INFO,"Turning backlight off");
-			start_fade(0);
-		}else{
-			// Otherwise, clear off_time_changed and wait until off_time
-			off_time_changed=false;
-			ret=pthread_cond_timedwait(&timer_cond,&timer_mutex,&off_time);
-		}
-	}
-}
-
 int main(int const argc,char *const *const argv){
 	is_daemon=!isatty(1);
 	
@@ -328,10 +303,6 @@ int main(int const argc,char *const *const argv){
 		exit(EXIT_FAILURE);
 	}
 	
-	// Set input_batch_delay to half configured_on_sec with an upper limit of 5 seconds
-	struct timespec input_batch_delay={};
-	timespec_add_nsec(&input_batch_delay,min((long)configured_on_sec*NSEC_PER_SEC/2,(long)NSEC_PER_SEC*5));
-	
 	log_write(LOG_INFO,"Backlight timeout: %d second%s",configured_on_sec,configured_on_sec!=1?"s":"");
 	log_write(LOG_INFO,"Brightness level: %d%%",configured_brightness);
 	log_write(LOG_INFO,"Fade duration: %d microsecond%s",configured_fade_duration_nsec/NSEC_PER_USEC,configured_fade_duration_nsec/NSEC_PER_USEC!=1?"s":"");
@@ -345,11 +316,6 @@ int main(int const argc,char *const *const argv){
 	pthread_t fader_thread;
 	pthread_create(&fader_thread,NULL,fader,&(struct fader_config){.brightness_file=brightness_file,.fade_duration_nsec=configured_fade_duration_nsec});
 	
-	pthread_cond_init(&timer_cond,&condattr);
-	pthread_mutex_init(&timer_mutex,NULL);
-	pthread_t timer_thread;
-	pthread_create(&timer_thread,NULL,timer,NULL);
-	
 	// Set up the exit handler
 	struct sigaction exit_action;
 	exit_action.sa_handler=exit_handler;
@@ -358,24 +324,23 @@ int main(int const argc,char *const *const argv){
 	
 	set_brightness(brightness_file,0);
 	
-	struct timespec latest_event_time={};
+	struct timespec off_time={};
 	while(true){
 		if(exit_requested){
 			break;
 		}
 		
-		if(poll(found_device_pollfds,found_device_count,-1)==-1){
+		if(poll(found_device_pollfds,found_device_count,desired_brightness==configured_brightness?0:-1)==-1){
 			if(errno!=EINTR){
 				log_write(LOG_ERR,"Poll failure‽");
 				exit(EXIT_FAILURE);
 			}
 		}
 		
-		bool new_event=false;
 		for(int device_index=0;device_index<found_device_count;device_index++){
 			if(found_device_pollfds[device_index].revents&POLLIN){
-				struct input_event input_event[512];
-				int read_bytes=read(found_device_pollfds[device_index].fd,input_event,sizeof(input_event));
+				struct input_event input_events[512];
+				int read_bytes=read(found_device_pollfds[device_index].fd,input_events,sizeof(input_events));
 				if(read_bytes==-1){
 					if(errno!=EINTR){
 						log_write(LOG_ERR,"Read failure‽");
@@ -386,14 +351,15 @@ int main(int const argc,char *const *const argv){
 				// The last event is always an EV_SYN, so start at the second-to-last and go backward…
 				for(int event_index=read_bytes/sizeof(struct input_event)-2;event_index>=0;event_index--){
 					// Looking for an event that is not EV_SYN or EV_LED…
-					if(input_event[event_index].type!=EV_SYN&&input_event[event_index].type!=EV_LED){
-						// If one is found, convert the event time into a timespec and update latest_event_time if it is more recent
-						struct timespec event_time;
-						event_time.tv_sec=input_event[event_index].input_event_sec;
-						event_time.tv_nsec=input_event[event_index].input_event_usec*NSEC_PER_USEC;
-						if(timespec_cmp(event_time,latest_event_time)>=0){
-							latest_event_time=event_time;
-							new_event=true;
+					if(input_events[event_index].type!=EV_SYN&&input_events[event_index].type!=EV_LED){
+						// If one is found, calculate candidate_off_time
+						struct timespec candidate_off_time;
+						candidate_off_time.tv_sec=input_events[event_index].input_event_sec+configured_on_sec;
+						candidate_off_time.tv_nsec=input_events[event_index].input_event_usec*NSEC_PER_USEC;
+						
+						// If candidate_off_time is later than off_time, update off_time
+						if(timespec_cmp(candidate_off_time,off_time)==1){
+							off_time=candidate_off_time;
 						}
 						break;
 					}
@@ -401,35 +367,29 @@ int main(int const argc,char *const *const argv){
 			}
 		}
 		
-		if(new_event){
-			pthread_mutex_lock(&timer_mutex);
-			// Set off_time to latest_event_time plus configured_on_sec
-			off_time.tv_sec=latest_event_time.tv_sec+configured_on_sec;
-			off_time.tv_nsec=latest_event_time.tv_nsec;
-			
-			// If the backlight is currently off…
+		struct timespec current_time;
+		clock_gettime(CLOCK_MONOTONIC,&current_time);
+		if(timespec_cmp(off_time,current_time)==1){
+			// If the off_time is in the future
 			if(desired_brightness==0){
-				// Turn it on
+				// And the backlight is currently off, turn it on
 				log_write(LOG_INFO,"Turning backlight on");
 				start_fade(configured_brightness);
-				
-				// And signal the timer thread to start timing down to turn it off
-				pthread_cond_signal(&timer_cond);
-			}else{
-				// Otherwise, set the flag to let the timer thread know that off_time changed
-				off_time_changed=true;
 			}
-			pthread_mutex_unlock(&timer_mutex);
 			
-			// Sleep here to prevent spinning and using too much CPU
-			nanosleep(&input_batch_delay,NULL);
+			// Sleep until off_time
+			struct timespec sleep_until=off_time;
+			timespec_sub(&sleep_until,current_time);
+			nanosleep(&sleep_until,NULL);
+		}else{
+			// Otherwise, turn it off
+			log_write(LOG_INFO,"Turning backlight off");
+			start_fade(0);
 		}
 	}
 	
 	pthread_cancel(fader_thread);
-	pthread_cancel(timer_thread);
 	pthread_join(fader_thread,NULL);
-	pthread_join(timer_thread,NULL);
 	set_brightness(brightness_file,0);
 	
 	return EXIT_SUCCESS;
