tekq/keylightc

commit bf71f2dcd3957ba7e10b9fecfef1e92d0947b2cd

Remove the `candidate_off_time` gymnastics
As far as I can tell, that condition is always going to evaluate to
`true`.  The input events from the kernel are chronological and the
clock is monotonic, so there is no way any event received at a
given time could possibly have a timestamp older than an event
received in the past.  Beyond that, the behavior of the code isn't
even different; in any case it is still going to `break` out of the
loop and execute the code to compare `off_time` to `current_time`
and set the brightness appropriately.  Even if the conditional did
evaluate to `false`, it wouldn't be acting any differently or
saving any time versus the `true` case, therefore it is more
efficient to eliminate it entirely.

keylightc.c
@@ -314,13 +314,9 @@ int main(int const argc,char *const *const argv){
 					for(int event_index=read_bytes/sizeof(struct input_event)-2;event_index>=0;event_index--){
 						// Looking for only interesting events (EV_MSC, EV_KEY, EV_ABS, and EV_SYN SYN_DROPPED (which may appear if the buffer overflows at just the wrong time))…
 						if(input_events[event_index].type==EV_MSC||input_events[event_index].type==EV_KEY||input_events[event_index].type==EV_ABS||(input_events[event_index].type==EV_SYN&&input_events[event_index].code==SYN_DROPPED)){
-							// If one is found, calculate candidate_off_time and update off_time if off_time is older
-							struct timespec candidate_off_time;
-							candidate_off_time.tv_sec=input_events[event_index].input_event_sec+timeout_sec;
-							candidate_off_time.tv_nsec=input_events[event_index].input_event_usec*NSEC_PER_USEC;
-							if(timespec_gt(candidate_off_time,off_time)){
-								off_time=candidate_off_time;
-							}
+							// If one is found, update off_time
+							off_time.tv_sec=input_events[event_index].input_event_sec+timeout_sec;
+							off_time.tv_nsec=input_events[event_index].input_event_usec*NSEC_PER_USEC;
 							break;
 						}
 					}

commit c5d4688225b9d94dea7ddb722312723badeb4373

Re-apply index optimizations
The bug where the backlight would turn off early was caused by
bf71f2dc because it disregarded the fact that their might be input
from multiple sources in the same period.  In this case, if the
input from the latter device (probably the trackpad) that was older
could overwrite newer input from the former device (usually the
keyboard).  Therefore, the optimizations related to picking an
index to start with should be safe to apply.

keylightc.c
@@ -310,8 +310,9 @@ int main(int const argc,char *const *const argv){
 						return EXIT_FAILURE;
 					}
 					
-					// The last event is always an EV_SYN (unless events were dropped), so start at the second-to-last and go backward…
-					for(int event_index=read_bytes/sizeof(struct input_event)-2;event_index>=0;event_index--){
+					// If more than one event was returned, the most recent event will be EV_SYN SYN_REPORT (unless events were dropped), so in that case start at the second-to-last and go backward so we see the actual event(s)…
+					int event_count=read_bytes/sizeof(struct input_event);
+					for(int event_index=event_count>=2?event_count-2:0;event_index>=0;event_index--){
 						// Looking for only interesting events (EV_MSC, EV_KEY, EV_ABS, and EV_SYN SYN_DROPPED (which may appear if the buffer overflows at just the wrong time))…
 						if(input_events[event_index].type==EV_MSC||input_events[event_index].type==EV_KEY||input_events[event_index].type==EV_ABS||(input_events[event_index].type==EV_SYN&&input_events[event_index].code==SYN_DROPPED)){
 							// If one is found, calculate candidate_off_time and update off_time if off_time is older